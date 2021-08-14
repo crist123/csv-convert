8df093e9f60e746e86501cc92da69f66ec271462
Andres Fajardo <cristianfajardo3@gmail.com>
2021-08-14 13:52:19 -0500

    commit initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # csv-convert
+Tool conversion csv to json or csv to sql
+
+# Run tool
+`node index.js --file /path/to/file.csv --format {json or sql} --name name_file`
---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const fs = require('fs');
+const path = require('path');
+const csv = require('fast-csv');
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv;
+
+// Si no encuentra los parámetros
+var format = argv.format || 'sql';
+
+if (!argv.file) throw "Argument 'file' not found, try execute using --file <value>";
+if (format == 'sql' && !argv.table) throw "Argument 'table' not found, try execute using --table <value>";
+if (format == 'sql' && !argv.columns) throw "Argument 'columns' not found, try execute using --columns <value>";
+
+var name = argv.name || 'exported';
+var sql = `INSERT INTO ${argv.table} (${argv.columns}) VALUES {value}`;
+var parts = format == 'sql' ? argv.columns.replace(/\(|\)| /g, '').split(',') : [];
+var values = [];
+
+console.info("Init parsing.....");
+
+fs.createReadStream(argv.file)
+    .pipe(csv.parse({ headers: true, delimiter: ';' }))
+    .on('error', error => console.error(error))
+    .on('data', row => {
+        try {
+
+            // Si es sql
+            if (format == 'sql') {
+                let valuesReg = [];
+
+                for (const p of parts) {
+                    valuesReg.push(row[p] ? '"' + `${row[p].replace(/,|\"/)}` + '"' : '""');
+                }
+
+                // if (valuesReg.length != 3) valuesReg.push('fallo');
+                values.push(`(${valuesReg.join(',')})`);
+            }
+            else if (format == 'json') {
+                values.push(row);
+            }
+
+            console.info(".");
+        } catch (error) {
+            console.error(error);
+        }
+    })
+    .on('end', (rowCount) => {
+        fs.writeFile(name + '.' + format,
+            format == 'sql' ? sql.replace('{value}', values.join(',\n')) : JSON.stringify(values, null, "\t"),
+            function (err) {
+                if (err) throw err;
+                console.log(`CSV Exported in ${name}.${format} file`);
+            });
+        console.log(`Parsed ${rowCount} rows`)
+    });
